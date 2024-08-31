0622a31ff070e39416371cb12afd8ee070c11d0b
A7ak <51486750+A7ak@users.noreply.github.com>
2024-08-31 15:58:40 +0530

    [JIRA-7007] feat : test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,3 +45,11 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 }
+
+tasks.register<Copy>("copyPrepareCommitMsgHook") {
+    description = "copy prepare commit msg hook to git hook"
+    group = "git hooks"
+    outputs.upToDateWhen { false }
+    from( "$rootDir/scripts/prepare-commit-msg")
+    into( "$rootDir/.git/hooks/")
+}
